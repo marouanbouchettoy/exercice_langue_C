a53af21ddebb1b9ccc4667924450acb8ea221878
Zineb3150 <zinebounnane43@gmail.com>
2022-09-07 17:23:39 +0100

    prog2 nezha

---
+++ prog nezha/main1.cpp
@@ -0,0 +1,17 @@
+
+07/09/22 17:20#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	const  float pi=3.14 ;
+	float rayon,var;
+	printf("donner le rayon du cercle:");
+	scanf("%f",&rayon);
+	var=pi*rayon;
+printf("le Circonférence du cercle est:%.2f",var);
+
+
+	return 0;
+}
+
