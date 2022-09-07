b5bee1b9dc8f7602c0041e35232ded8187cd8d63
YAS15mine <fadwacherqui@gmail.com>
2022-09-07 06:50:01 -0700

    solution ex5 td2

---
+++ ex5s2/ex5s2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* echelle de richter */
+
+int main() {
+	int N;
+	char *T[10]={"Micron","Tresmineur","Mineur","Leger","Fort","Majeur","Important","Devasrareur"};
+ 	printf(" saisire une valeur pour avoir la discription de ce nombre dans l eechelle de richeter");
+    scanf("%d",&N);
+	if(N>0&&N<10)
+	{
+	   printf("la discription est :%s",T[N-1]);
+	}else {
+		printf("erreur de saisire il faut que N compris entre 1 et 9");
+	}
+	
+	return 0;
+}
