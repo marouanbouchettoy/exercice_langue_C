7de9f8bbb2887fdfd7298329271f9dca7c43ec7d
Fadwa Cherqui <111055828+fadwa2022@users.noreply.github.com>
2022-09-07 06:54:04 -0700

    Update ex5s2.c

--- ex5s2/ex5s2.c
+++ ex5s2/ex5s2.c
@@ -8,9 +8,9 @@ int main() {
 	char *T[10]={"Micron","Tresmineur","Mineur","Leger","Fort","Majeur","Important","Devasrareur"};
  	printf(" saisire une valeur pour avoir la discription de ce nombre dans l eechelle de richeter");
     scanf("%d",&N);
-	if(N>0&&N<10)
+	if(N>0 && N<10)
 	{
-	   printf("la discription est :%s",T[N-1]);
+	   printf("la discription est: %s",T[N-1]);
 	}else {
 		printf("erreur de saisire il faut que N compris entre 1 et 9");
 	}
