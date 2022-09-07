e9ddd622af9fc7194abb96d7fca079389688ec63
Zineb3150 <zinebounnane43@gmail.com>
2022-09-07 15:37:38 +0100

    prog nezha

---
+++ prog nezha/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#define n 40 
+int main() {
+	const int hh=2;
+	int tab[n],taille,i;
+	printf("donner la taille du tableau\n ",n);
+	scanf("%d",&taille);
+	for(i=0;i<taille;i++){
+		printf("donner la valeur %d du tableau ",i);
+    	scanf("%d",&tab[i]);
+	}
+	for(i=0;i<taille;i++){
+			printf("%d \n",tab[i]);	
+	}
+	printf("%d",hh);
